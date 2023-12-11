cc81f4ce2a37769c44e2cb92b677c541e3262876
asarmiento-neostella <andres.sarmiento@neostella.com>
2023-12-11 17:12:39 -0500

    improving day 11

--- day-11.js
+++ day-11.js
@@ -1,21 +1,29 @@
 function getIndexsForPalindrome(word) {
-  function checkIfPalindrome(wordToTest, length) {
-    for (let x = 0; x < Math.floor(length / 2); x++) {
-      if (wordToTest[x] !== wordToTest[length - 1 - x]) return false;
+  if (checkIfPalindrome(word)) return [];
+
+  for (let i = 0; i < word.length - 1; i++) {
+    for (let j = word.length - 1; j > i; j--) {
+      const modifiedWord =
+        word.substring(0, i) +
+        word[j] +
+        word.substring(i + 1, j) +
+        word[i] +
+        word.substring(j + 1);
+      if (checkIfPalindrome(modifiedWord)) {
+        return [i, j];
+      }
+    }
+  }
+
+  function checkIfPalindrome(wordToTest) {
+    const length = wordToTest.length;
+    for (let start = 0, end = length - 1; start < end; start++, end--) {
+      if (wordToTest[start] !== wordToTest[end]) {
+        return false;
+      }
     }
     return true;
   }
-  const splited = word.split("");
-  const length = word.length;
-  if (checkIfPalindrome(splited, length)) return [];
-  for (let i = 0; i < length - 1; i++) {
-    for (let j = i + 1; j < length; j++) {
-      splited[i] = word[j];
-      splited[j] = word[i];
-      if (checkIfPalindrome(splited, length)) return [i, j];
-      splited[i] = word[i];
-      splited[j] = word[j];
-    }
-  }
+
   return null;
 }
